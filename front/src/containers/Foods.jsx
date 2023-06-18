68e64d577bbbe8aa6b23da54cb3fda3b8595b827
ooooose <yuuki.oose1990@gmail.com>
2023-06-18 17:09:02 +0900

    [Add]foods一覧の取得と表示

--- front/src/containers/Foods.jsx
+++ front/src/containers/Foods.jsx
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-export const Foods = ({match}) => {
-    return (
-        <>
-            フード一覧
-        </>
-    )
+import { COLORS } from '../style_constants';
+import { LocalMallIcon } from '../components/Icons';
+
+import { FoodWrapper } from '../components/FoodWrapper';
+import { Skeleton } from '@mui/material';
+
+import { fetchFoods } from '../apis/foods';
+
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
+import {
+  initialState as foodsInitialState,
+  foodsActionTypes,
+  foodsReducer,
+} from '../reducers/foods';
+
+import { REQUEST_STATE } from '../constants';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
+
+export const Foods = () => {
+  const location = useLocation();
+  console.log(location);
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+  useEffect(() => {
+    dispatch({type: foodsActionTypes.FETCHING});
+    fetchFoods(1)
+    .then((data) => {
+      dispatch({
+        type: foodsActionTypes.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods
+        }
+      });
+    })
+  }, []);
+
+  return (
+    <>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </>
+          :
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  onClickFoodWrapper={(food) => console.log(food)}
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodsList>
+    </>
+  )
 }

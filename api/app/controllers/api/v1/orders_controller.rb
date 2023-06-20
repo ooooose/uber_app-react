7d48640b79d55fa0dd93bae6db961a5b05180cd0
ooooose <yuuki.oose1990@gmail.com>
2023-06-21 07:53:19 +0900

    [Fix]タイポ修正

--- api/app/controllers/api/v1/orders_controller.rb
+++ api/app/controllers/api/v1/orders_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::OrdersController < ApplicationController
   def create
-    posted_lline_foods = LineFood.where(id: params[:line_food_ids])
+    posted_line_foods = LineFood.where(id: params[:line_food_ids])
     order = Order.new(
       total_price: total_price(posted_line_foods),
     )
